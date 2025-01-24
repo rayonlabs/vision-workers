ff32f1e4cc1a4652d0e7b5fa3f595d37aeb39828
MahdiPresario001 <mehdi.lko001@gmail.com>
2025-01-24 13:17:49 +0100

    debug

--- validator_orchestrator/app/checking/functions/text.py
+++ validator_orchestrator/app/checking/functions/text.py
@@ -84,7 +84,6 @@ async def _tokenize_and_detokenize(input_payload: dict, model_name: str, eos_tok
             last_eos_index = max((index for index, value in enumerate(token_list) if value == 151643), default=None)
             if last_eos_index is not None:
                 token_list = token_list[:last_eos_index]
-            
 
         detokenize_response = await http_client.post(url=f"{BASE_URL}/detokenize", json={"tokens": token_list, "model": model_name})
         detokenize_response.raise_for_status()
@@ -254,6 +253,9 @@ async def check_text_result(result: models.QueryResult, payload: dict, task_conf
 
     failed_tokens_idx = []
     failed_tokens_details = []
+
+    logger.info(f"all_tokens[num_input_tokens:4] : \n{all_tokens[num_input_tokens:4]}")
+    logger.info(f"messages[num_input_tokens:4] : \n{messages[num_input_tokens:4]}")
 
     for idx, response_token, logprobs in zip(range(len(all_tokens[num_input_tokens:])), all_tokens[num_input_tokens:], prompt_logprobs):
         # Just a helper for nicer printing
