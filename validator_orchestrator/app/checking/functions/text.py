e9c3f05cc02d8a748bebcc4f35467d37ccbdaf0f
MahdiPresario001 <mehdi.lko001@gmail.com>
2025-04-04 06:00:04 +0200

    debug

--- validator_orchestrator/app/checking/functions/text.py
+++ validator_orchestrator/app/checking/functions/text.py
@@ -186,7 +186,7 @@ async def calculate_distance_for_token(
         "temperature": llm_request.temperature,
         "top_p": 1,
         "max_tokens": 1,
-        "logprobs": 20,
+        "logprobs": 20 if llm_request.temperature <= 0.5 else int(20 / (1.03 - llm_request.temperature)),
         "add_special_tokens": False
     }
     try:
@@ -321,7 +321,7 @@ async def check_text_result(result: models.QueryResult, payload: dict, task_conf
     failed_tokens_idx = []
     failed_tokens_details = []
 
-    max_acceptable_rank = 10 if payload["temperature"] <= 0.5 else 10 / (1.03 - payload["temperature"])
+    max_acceptable_rank = 10 if payload["temperature"] <= 0.5 else int(10 / (1.03 - payload["temperature"]))
 
     for idx, response_token, logprobs in zip(range(len(all_tokens[num_input_tokens:])), all_tokens[num_input_tokens:], prompt_logprobs):
         # Just a helper for nicer printing
