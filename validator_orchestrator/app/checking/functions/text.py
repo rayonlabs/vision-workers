14785fe0465e074d0e9d5caeee3d2f0a21761b95
Arpan Tripathi <tripathiarpan20@gmail.com>
2025-04-18 11:43:33 +0100

    Update text.py

--- validator_orchestrator/app/checking/functions/text.py
+++ validator_orchestrator/app/checking/functions/text.py
@@ -21,7 +21,7 @@ MAX_FAILED_TOKENS = 5
 MAX_CHECKS = 5
 
 DEFAULT_NUM_LOGPROBS = 20
-DEFAULT_PROMPT_LOGPROBS = 9 #vllm 0.8.3 breaks with prompt_logprobs > 9
+DEFAULT_PROMPT_LOGPROBS = 1 #vllm 0.8.3 breaks at some arbitrary prompt_logprobs > 1: https://github.com/vllm-project/vllm/issues/16836 
 
 MODELS_FOR_EOT_HANDLING = ["llama-3", "deepseek-r1", "qwq-32b", "qwen2.5-7b"]
 
