d85f8d13fa361bb356cbb5f7de07750c9379ee24
Arpan Tripathi <tripathiarpan20@gmail.com>
2025-05-26 11:28:31 +0100

    Update text.py

--- validator_orchestrator/app/checking/functions/text.py
+++ validator_orchestrator/app/checking/functions/text.py
@@ -92,8 +92,8 @@ async def _detokenize(tokens: list[int], model: str) -> str:
 
 async def _tokenize_and_detokenize(input_payload: dict, model_name: str, eos_token_id: int, add_generation_prompt: bool = True) -> tuple[str, int]:
     async with httpx.AsyncClient() as http_client:
-        logger.info(f"Tokenizing at: {BASE_URL}/tokenize")
-        tokenize_response = await http_client.post(url=f"{BASE_URL}/tokenize", json=input_payload)
+        logger.info(f"Tokenizing at: {CHUTES_BASE_URL if model_name in chutes_checking_supported_models else BASE_URL}/tokenize")
+        tokenize_response = await http_client.post(url=f"{CHUTES_BASE_URL if model_name in chutes_checking_supported_models else BASE_URL}/tokenize", json=input_payload)
         tokenize_response.raise_for_status()
         token_list: list[int] = tokenize_response.json()["tokens"]
 
@@ -103,7 +103,7 @@ async def _tokenize_and_detokenize(input_payload: dict, model_name: str, eos_tok
                 token_list = token_list[:last_eot_index]
 
         detokenize_response = await http_client.post(
-            url=f"{BASE_URL}/detokenize", 
+            url=f"{CHUTES_BASE_URL if model_name in chutes_checking_supported_models else BASE_URL}/detokenize", 
             json={"tokens": token_list, "model": model_name}
         )
         detokenize_response.raise_for_status()
