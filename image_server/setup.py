28300ab5c7a5955582e25bfec13305e80cd716c0
diagonalge <diagonalge@gmail.com>
2025-07-14 17:47:49 +0000

    change text encoder to fp16

--- image_server/setup.py
+++ image_server/setup.py
@@ -85,10 +85,10 @@ def main():
         cache_dir="ComfyUI/models/caches",
     )
 
-    logger.info("Downloading t5xxl_fp8_e4m3fn.safetensors")
+    logger.info("Downloading t5xxl_fp16.safetensors")
     download_file(
         repo_id="comfyanonymous/flux_text_encoders",
-        filename="t5xxl_fp8_e4m3fn.safetensors",
+        filename="t5xxl_fp16.safetensors",
         local_dir="ComfyUI/models/text_encoders",
         cache_dir="ComfyUI/models/caches",
     )
